35e07ec5a3b193747c7861211f79d8723de0e8bb
Aul Wardana <auldesain@gmail.com>
2017-11-24 15:08:10 +0700

    create metering chart for each pashe theme

--- mcp-t.php
+++ mcp-t.php
@@ -8,6 +8,104 @@ include_once('includes/_top.php');
             include_once('includes/_sidebar.php');
             ?>
             <!--BEGIN CONTENT-->
+            <div id="main-content">
+                <div class="container-fluid">
+                    <div class="row-fluid">
+                        <div class="span12">
+                            <h3 class="page-title">
+                                Metering Chart Phase T
+                            </h3>
+                            <ul class="breadcrumb">
+                                <li>
+                                    <i class="icon-home"></i><span class="divider">&nbsp;</span>
+                                </li>
+                                <li>
+                                    Microhydro Monitoring<span class="divider">&nbsp;</span>
+                                </li>
+                                <li>
+                                    Metering Chart Phase T<span class="divider-last">&nbsp;</span>
+                                </li>
+                                <li class="pull-right search-wrap"></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div id="page" class="dashboard">
+                        <div class="row-fluid">
+                            <div class="widget">
+                                <div class="widget-title">
+                                    <h4><i class="icon-bolt"></i> Real Power Meter</h4>
+                                    <span class="tools">
+                                    <a href="javascript:;" class="icon-chevron-down"></a>
+                                    <a href="javascript:;" class="icon-remove"></a>
+                                    </span>
+                                </div>
+                                <div class="widget-body">
+                                    <div id="site_statistics_content" class="show">
+                                        <div id="placeholder" class="chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <div class="widget-title">
+                                    <h4><i class="icon-bolt"></i> Apparent Power Meter</h4>
+                                    <span class="tools">
+                                    <a href="javascript:;" class="icon-chevron-down"></a>
+                                    <a href="javascript:;" class="icon-remove"></a>
+                                    </span>
+                                </div>
+                                <div class="widget-body">
+                                    <div id="site_statistics_content" class="show">
+                                        <div id="placeholder2" class="chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <div class="widget-title">
+                                    <h4><i class="icon-bolt"></i> Power Factor Meter</h4>
+                                    <span class="tools">
+                                    <a href="javascript:;" class="icon-chevron-down"></a>
+                                    <a href="javascript:;" class="icon-remove"></a>
+                                    </span>
+                                </div>
+                                <div class="widget-body">
+                                    <div id="site_statistics_content" class="show">
+                                        <div id="placeholder3" class="chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <div class="widget-title">
+                                    <h4><i class="icon-bolt"></i> Voltage Meter</h4>
+                                    <span class="tools">
+                                    <a href="javascript:;" class="icon-chevron-down"></a>
+                                    <a href="javascript:;" class="icon-remove"></a>
+                                    </span>
+                                </div>
+                                <div class="widget-body">
+                                    <div id="site_statistics_content" class="show">
+                                        <div id="placeholder4" class="chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <div class="widget-title">
+                                    <h4><i class="icon-bolt"></i> Ampere Meter</h4>
+                                    <span class="tools">
+                                    <a href="javascript:;" class="icon-chevron-down"></a>
+                                    <a href="javascript:;" class="icon-remove"></a>
+                                    </span>
+                                </div>
+                                <div class="widget-body">
+                                    <div id="site_statistics_content" class="show">
+                                        <div id="placeholder5" class="chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!--FINISH CONTENT-->
         </div>
 
