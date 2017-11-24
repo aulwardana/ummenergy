46b1c14cb1fb40ab2a96d406016bc6946cbcccd5
Aul Wardana <auldesain@gmail.com>
2017-11-24 15:43:22 +0700

    change menu

--- includes/_sidebar.php
+++ includes/_sidebar.php
@@ -2,20 +2,20 @@
                 <div class="sidebar-toggler hidden-phone"></div>
                     <ul class="sidebar-menu">
                         <li class="has-sub">
-                        <li><a class="" href="index.php"><span class="icon-box"><i class="icon-tasks"></i></span> Dashboard</a></li>
+                        <li><a class="" href="dashboard.php"><span class="icon-box"><i class="icon-tasks"></i></span> Dashboard</a></li>
                         <li class="has-sub">
                         <a href="javascript:;" class="">
                                 <span class="icon-box"> <i class="icon-dashboard"></i></span> Monitoring
                                 <span class="arrow"></span>
                             </a>
                             <ul class="sub">
-                                <li><a class="" href="metering.php">Metering Chart Phase R</a></li>
-                                            <li><a class="" href="meterings.php">Metering Chart Phase S</a></li>
-                                            <li><a class="" href="meteringt.php">Metering Chart Phase T</a></li>
-                                <li><a class="" href="statistik.php">Statistic Chart</a></li>
-                                <li><a class="" href="data.php">Data Report Phase R</a></li>
-                                <li><a class="" href="datas.php">Data Report Phase S</a></li>
-                                <li><a class="" href="datat.php">Data Report Phase T</a></li>
+                                <li><a class="" href="mcp-r.php">Metering Chart Phase R</a></li>
+                                <li><a class="" href="mcp-s.php">Metering Chart Phase S</a></li>
+                                <li><a class="" href="mcp-t.php">Metering Chart Phase T</a></li>
+                                <li><a class="" href="statistic.php">Statistic Chart</a></li>
+                                <li><a class="" href="drp-r.php">Data Report Phase R</a></li>
+                                <li><a class="" href="drp-s.php">Data Report Phase S</a></li>
+                                <li><a class="" href="drp-t.php">Data Report Phase T</a></li>
                             </ul>
                         </li>
                         <li class="has-sub">
@@ -26,7 +26,7 @@
                             <ul class="sub">
                                 <li><a class="" href="settings.php">Monitoring Settings</a></li>
                                 <li><a class="" href="register.php">Register New User</a></li>
-                                <li><a class="" href="account.php">Manage Account</a></li>
+                                <li><a class="" href="users.php">Manage Account</a></li>
                             </ul>
                         </li>
                     </ul>
