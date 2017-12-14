640ec1c11309a50cb9c5027f4fb4c8375810a25d
Aul Wardana <auldesain@gmail.com>
2017-12-14 15:14:56 +0700

    add user access role

--- settings.php
+++ settings.php
@@ -4,8 +4,12 @@ include_once('includes/_top.php');
 include_once('includes/Mail.php');
 include_once('includes/Mail/mime.php');
 
-$user_id = $_SESSION['user_id'];
-$time = gmdate("Y-m-d\TH:i:s\Z");
+if ($role == "Administrator") {
+    $user_id = $_SESSION['user_id'];
+    $time = gmdate("Y-m-d\TH:i:s\Z");
+} else {
+    header ("location:index.php");
+}
 
 if (isset($_POST['backup'])) {
     $dbhost = Config::read('db.host');
@@ -17,67 +21,67 @@ if (isset($_POST['backup'])) {
     $sendsubject = "Daily Database Backup";
     $bodyofemail = "Here is the backup for $time.";
 
-	backup_database_tables($dbhost,$dbuser,$dbpass,$dbname, '*');
-	
-	function backup_database_tables($host,$user,$pass,$name,$tables){
-	     
-	    $link = mysql_connect($host,$user,$pass);
-	    mysql_select_db($name,$link);
-	     
-	    //get all of the tables
-	    if($tables == '*'){
-	        $tables = array();
-	        $result = mysql_query('SHOW TABLES');
-	        while($row = mysql_fetch_row($result)){
-	            $tables[] = $row[0];
-	        }
-	    }
-	    else{
-	        $tables = is_array($tables) ? $tables : explode(',',$tables);
-	    }
-	     
-	    //cycle through each table and format the data
-	    foreach($tables as $table){
-	        $result = mysql_query('SELECT * FROM '.$table);
-	        $num_fields = mysql_num_fields($result);
-	         
-	        $return.= 'DROP TABLE '.$table.';';
-	        $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
-	        $return.= "\n\n".$row2[1].";\n\n";
-	         
-	        for ($i = 0; $i < $num_fields; $i++){
-	            while($row = mysql_fetch_row($result)){
-	                $return.= 'INSERT INTO '.$table.' VALUES(';
-	                for($j=0; $j<$num_fields; $j++){
-	                    $row[$j] = addslashes($row[$j]);
-	                    $row[$j] = ereg_replace("\n","\\n",$row[$j]);
-	                    if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
-	                    if ($j<($num_fields-1)) { $return.= ','; }
-	                }
-	                $return.= ");\n";
-	            }
-	        }
-	        $return.="\n\n\n";
-	    }
-	     
-	    //save the file
-	    $handler = fopen(date("Y-m-d").'.sql','w+');
-	    fwrite($handler,$return);
-	    fclose($handler);
-	}
-	$backupfile = date("Y-m-d").'.sql';
+    backup_database_tables($dbhost,$dbuser,$dbpass,$dbname, '*');
+    
+    function backup_database_tables($host,$user,$pass,$name,$tables){
+        
+        $link = mysql_connect($host,$user,$pass);
+        mysql_select_db($name,$link);
+        
+        //get all of the tables
+        if($tables == '*'){
+            $tables = array();
+            $result = mysql_query('SHOW TABLES');
+            while($row = mysql_fetch_row($result)){
+                $tables[] = $row[0];
+            }
+        }
+        else{
+            $tables = is_array($tables) ? $tables : explode(',',$tables);
+        }
+        
+        //cycle through each table and format the data
+        foreach($tables as $table){
+            $result = mysql_query('SELECT * FROM '.$table);
+            $num_fields = mysql_num_fields($result);
+            
+            $return.= 'DROP TABLE '.$table.';';
+            $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+            $return.= "\n\n".$row2[1].";\n\n";
+            
+            for ($i = 0; $i < $num_fields; $i++){
+                while($row = mysql_fetch_row($result)){
+                    $return.= 'INSERT INTO '.$table.' VALUES(';
+                    for($j=0; $j<$num_fields; $j++){
+                        $row[$j] = addslashes($row[$j]);
+                        $row[$j] = ereg_replace("\n","\\n",$row[$j]);
+                        if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                        if ($j<($num_fields-1)) { $return.= ','; }
+                    }
+                    $return.= ");\n";
+                }
+            }
+            $return.="\n\n\n";
+        }
+        
+        //save the file
+        $handler = fopen(date("Y-m-d").'.sql','w+');
+        fwrite($handler,$return);
+        fclose($handler);
+    }
+    $backupfile = date("Y-m-d").'.sql';
     
     //mail sender
-	$message = new Mail_mime();
-	$text = "$bodyofemail";
-	$message->setTXTBody($text);
-	$message->AddAttachment($backupfile);
-	$body = $message->get();
-	$extraheaders = array("From"=>"$sendfrom", "Subject"=>"$sendsubject");
-	$headers = $message->headers($extraheaders);
-	$mail = Mail::factory("mail");
-	$mail->send("$sendto", $headers, $body);
-	
+    $message = new Mail_mime();
+    $text = "$bodyofemail";
+    $message->setTXTBody($text);
+    $message->AddAttachment($backupfile);
+    $body = $message->get();
+    $extraheaders = array("From"=>"$sendfrom", "Subject"=>"$sendsubject");
+    $headers = $message->headers($extraheaders);
+    $mail = Mail::factory("mail");
+    $mail->send("$sendto", $headers, $body);
+    
     unlink($backupfile);
 
     $prep_stmt_db = "UPDATE account SET date_backup_db = :date_backup_db WHERE id_account = :id";
@@ -88,7 +92,7 @@ if (isset($_POST['backup'])) {
         header('Location: ./settings.php');
         exit();
     }
-}
+} 
 ?>
 
         <div id="container" class="row-fluid">
