51677a21987209e46cd06dba4e1cf17d75194761
Aul Wardana <auldesain@gmail.com>
2017-12-16 17:08:26 +0700

    change mail model for notification

--- sensing/_notification.php
+++ sensing/_notification.php
@@ -1,17 +1,15 @@
 <?php
    include("../includes/_connect-db.php");
-   include_once('Mail.php');
-   include_once('Mail/mime.php');
 
    $core = Core::getInstance();
    $status = "1";
 
    if (isset($_POST['notif'])) {
         $notif = filter_input(INPUT_POST, 'notif', FILTER_SANITIZE_STRING);
-        
-        $sendfrom = "Platform Notif <notification@ummenergy.com>";
-        $sendsubject = "Device Notification";
-        $bodyofemail = "Notification : $notif.";
+
+        $subject = "Notification UMMEnergy";
+        $message = $notif;
+        $header = "From:notification@ummenergy.com \r\n";
 
         if ($stmt = $core->dbh->prepare("SELECT email FROM account WHERE notification = :statuss")) {
             $stmt->bindParam(':statuss', $status);
@@ -22,16 +20,8 @@
  
               foreach($row as $row){
                   $email=$row[0];
-                  $sendto = "Webmaster <$email>";
-                  $message = new Mail_mime();
-                  $text = "$bodyofemail";
-                  $message->setTXTBody($text);
-                  $message->AddAttachment($backupfile);
-                  $body = $message->get();
-                  $extraheaders = array("From"=>"$sendfrom", "Subject"=>"$sendsubject");
-                  $headers = $message->headers($extraheaders);
-                  $mail = Mail::factory("mail");
-                  $mail->send("$sendto", $headers, $body);
+                  $to = $email;
+                  mail ($to,$subject,$message,$header);
               }
             }
         }
